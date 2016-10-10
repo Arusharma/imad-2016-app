ba39b671190302e00f04b23407df878580da65b5
Arusharma <soni.aru.sharma@gmail.com>
2016-10-11 00:07:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function (req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -94,12 +101,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-var counter=0;
-app.get('/counter',function (req,res)
-{
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
