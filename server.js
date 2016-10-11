a7d8c6ae5bc7b9d9a8ef5889bcb7dfb96c5d6868
Arusharma <soni.aru.sharma@gmail.com>
2016-10-11 12:57:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,12 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-
+var names=[];
+app.get('/submit-name/:name',function(req,res){
+   var name=req.params.name;
+   names.push(name);
+   res.send(JSON.stringify(names));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
