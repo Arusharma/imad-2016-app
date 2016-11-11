0151cd512317dfccce16667d01f910cb416e40e2
Arusharma <soni.aru.sharma@gmail.com>
2016-11-11 21:51:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,162 +1,350 @@
 var express = require('express');
+
 var morgan = require('morgan');
+
 var path = require('path');
+
 var Pool = require('pg').Pool;
 
 var config = {
-  host: 'db.imad.hasura-app.io',
+
+    host: 'db.imad.hasura-app.io',
+
+  user: 'mounya246',
+
   port:'5432',
-  user: 'arusharma',
-  database: 'arusharma',
-  password: process.env.DB_PASSWORD
+
+  password: process.env.DB_PASSWORD,
+
+  database: 'mounya246',
+
 };
+
 var app = express();
+
 app.use(morgan('combined'));
 
+
+
+
+
+
+
+
+
+
 var articles=
+
 {
+
+    
+
         'article-one':{
-            title:'Article One|Arunima Sharma',
+
+            title:'Article One|MOUNYA',
+
             heading:'Article One',
+
             date:'10th October 2016',
+
             content:`<p>
+
                         This is the content for my first article..
+
                     </p>
+
                     <p>
+
                         I am currently working in imad console.
+
                     </p>`
+
 },
+
 'article-two':{
-    title:'Article Two|Arunima Sharma',
+
+    title:'Article Two|mounya',
+
             heading:'Article Two',
+
             date:'10th October 2016',
+
             content:`<p>
+
                         This is the content for my second article..
+
                     </p>
+
                     <p>
+
                         I am currently working in imad console.
+
                     </p>`
+
 },
+
 'article-three':{
-    title:'Article Three|Arunima Sharma',
+
+    title:'Article Three|mounya',
+
             heading:'Article Three',
+
             date:'10th October 2016',
+
             content:`<p>
+
                         This is the content for my third article..
+
                     </p>
+
                     <p>
+
                         I am currently working in imad console.
-                    </p>
+
+                    </p>`
+
 }
+
 };
+
+
 
 
 function createTemplate(data){
+
     var title=data.title;
+
     var heading=data.heading;
+
     var date=data.date;
+
     var content=data.content;
 
+
+
+
 var htmlTemplate=`
+
 <html>
+
 <head>
+
     <title>
+
         ${title}
+
     </title>
+
     <link href="/ui/style.css" rel="stylesheet" />
+
 </head>
+
 <body>
+
     <div class="container">
+
         <div>
+
             <a href="/">Home</a>
+
         </div>
+
         <hr/>
+
         <h3>
+
             ${heading}
+
         </h3>
+
         <div>
+
            ${date.toDateString()}
+
         </div>
+
         <div>
+
            ${content}
+
         </div>
+
     </div>
+
 </body>
+
 </html>
+
 `;
+
 return htmlTemplate;
+
 }
+
     
+
+
+
+
+
 
 
 app.get('/', function (req, res) {
+
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
 });
 
 var pool = new Pool(config);
+
+
+
+
 app.get('/test-db',function(req,res)
+
 {
-    pool.query('SELECT * FROM test',function(err,result)
+
+    pool.query('SELECT*FROM test',function(err,result)
+
     {
+
         if(err)
+
         {res.status(500).send(err.toString());
-        }
-        else
+
+        }else
+
         {
+
             res.send(JSON.stringify(result.rows));
+
         }
+
     });
+
+    
+
 });
+
 var counter=0;
+
 app.get('/counter',function (req,res)
+
 {
+
     counter=counter+1;
+
     res.send(counter.toString());
+
 });
+
+
+
 
 app.get('/ui/style.css', function (req, res) {
+
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+
 });
+
+
+
 
 var names=[];
+
 app.get('/submit-name',function(req,res){
+
    var name=req.query.name;
+
    names.push(name);
+
    res.send(JSON.stringify(names));
+
 });
 
+
+
+
 app.get('/articles/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
+
     
-    pool.query("SELECT * FROM article WHERE title ='"+ req.params.articleName +"'",function(err,result)
+
+    pool.query("SELECT * FROM article WHERE title ='"+req.params.articleName +"'",function(err,result)
+
     {
+
         if(err)
+
         {
+
             res.status(500).send(err.toString());
+
+            
+
         }else
+
         {
+
             if(result.rows.length===0)
+
             {
+
                 res.status(404).send('article not found');
-            }else
-            {
-                var articleData=result.rows[0];
-                res.send(createTemplate(articleData));
-            }
+
+            
+
+             }
+
+             else
+
+             {
+
+                 var articleData=result.rows[0];
+
+                 res.send(createTemplate(articleData));
+
+             }
+
+             
+
         }
+
     });
+
+    
+
+  
+
 });
+
+
+
+
+
 
 
 app.get('/ui/madi.png', function (req, res) {
+
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
 });
+
+
+
 
 app.get('/ui/main.js', function (req, res) {
+
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+
 });
 
+
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+
 app.listen(8080, function () {
+
   console.log(`IMAD course app listening on port ${port}!`);
+
 });
