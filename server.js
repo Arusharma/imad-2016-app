d6a47bfdaddaf05627f891c24ce3a1de242ca03c
Arusharma <soni.aru.sharma@gmail.com>
2016-11-11 23:38:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path = require('path');
 
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config = {
     host: 'db.imad.hasura-app.io',
 
@@ -112,7 +113,7 @@ app.post('/create-user',function(req,res)
 {
     var username=req.body.username;
     var password=req.body.password;
-    var salt=crpyto.getRandomBytes(128).toString('hex');
+    var salt=crypto.getRandomBytes(128).toString('hex');
      var dbString=hash(password, salt);
      pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result)
      {
