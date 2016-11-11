f6d4846d435470c1706174e10543bac3ccce94d4
Arusharma <soni.aru.sharma@gmail.com>
2016-11-11 21:24:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/articles/:articleName', function (req, res) {
             res.status(500).send(err.toString());
         }else
         {
-            if(result.rows.length=='0')
+            if(result.rows.length===0)
             {
                 res.status(404).send('article not found');
             }else
@@ -110,8 +110,6 @@ app.get('/articles/:articleName', function (req, res) {
             }
         }
     });
-    
-  res.send(createTemplate(articles[articleName]));
 });
 
 
